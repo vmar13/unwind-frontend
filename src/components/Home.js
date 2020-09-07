d258f4fb64929d07cc508f8c1271d08057d9198d
github username <60378440+vmar13@users.noreply.github.com>
2020-09-07 12:55:35 -0400

    added login form

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,15 @@ class Home extends React.Component {
     render(){
         return(
             <>
+            <div className='login-form'>
                 <h2 id='home-title'>Start to Unwind</h2>
+                <form>
+                    <input type='text' placeholder='Username'/><br/>
+                    <input type='password' placeholder='Password'/><br/>
+                    <input type='submit' />
+                </form>
+            </div>
+                
                 <div className='home-img-container'>
                     <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' alt='home' className='home-img' />
                 </div>
