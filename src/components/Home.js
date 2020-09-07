9ce899b7362f50609876ad90d327bc6e6c991114
github username <60378440+vmar13@users.noreply.github.com>
2020-09-06 23:17:50 -0400

    added div container to home img to try to center

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,11 @@ import React from 'react'
 class Home extends React.Component {
     render(){
         return(
-            <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' className='home-img' />
+            <>
+                <div className='home-img-container'>
+                    <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' className='home-img' />
+                </div>
+            </>
             )
     }
 }
