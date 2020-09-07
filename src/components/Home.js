68848c00451beffe6e287d403e9cf185e63dc210
github username <60378440+vmar13@users.noreply.github.com>
2020-09-06 23:55:33 -0400

    added home header

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ class Home extends React.Component {
     render(){
         return(
             <>
+                <h2 id='home-title'>Start to Unwind</h2>
                 <div className='home-img-container'>
                     <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' alt='home' className='home-img' />
                 </div>
