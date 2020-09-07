6e582eb79a3ef599c2c9b8d00e1f5c0151720005
github username <60378440+vmar13@users.noreply.github.com>
2020-09-06 23:49:15 -0400

    centered home img

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component {
         return(
             <>
                 <div className='home-img-container'>
-                    <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' className='home-img' />
+                    <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' alt='home' className='home-img' />
                 </div>
             </>
             )
