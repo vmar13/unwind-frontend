bb9a8511662d0e4130e3b6693f17b9c80e5c6b3a
github username <60378440+vmar13@users.noreply.github.com>
2020-09-14 21:50:13 -0400

    adjusted anim circle colors and speed

--- src/components/Home.js
+++ src/components/Home.js
@@ -50,8 +50,6 @@ class Home extends React.Component {
 
                 </form>
             </div>
-
-            <button>Apply</button>
                 
                 <div className='home-img-container'>
                     <img src='https://images.unsplash.com/photo-1538495435388-104fd74d46a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2841&q=80' alt='home' className='home-img' />
