426dc11fc45786895743d2fcf4ea9fee980f4b47
github username <60378440+vmar13@users.noreply.github.com>
2020-12-16 18:02:06 -0500

    cleaned up breathingTech comp

--- src/components/BreathingTech.js
+++ src/components/BreathingTech.js
@@ -195,31 +195,6 @@ class BreathingTech extends React.Component {
     //     }
     // }
 
-    removeDuplicates = nums => {
-        if(nums.length === 0) return 0
-        
-        let p1 = 0
-        for(let p2 = 1; p2 < nums.length; p2++){
-            if(nums[p1] !== nums[p2]){
-                p1++
-                nums[p1] = nums[p2]
-            }
-        }
-        return p1 + 1
-    }
-
-    removeElement = function(nums, val) {
-        if(nums.length === 0) return 0
-        
-        let i = 0
-        for(let j = 0; j < nums.length; j++){
-            if(nums[i] === val){
-                nums[i] = nums.length - 1
-            }
-        }
-     return nums.length
-    }
-
     componentDidMount(){
        this._isMounted = true 
        this.getOneBreathingTech()
