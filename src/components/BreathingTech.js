430c135baf195a10aab30f48195a004d306b2d37
github username <60378440+vmar13@users.noreply.github.com>
2020-12-16 18:01:09 -0500

    added removeDupes for favorites

--- src/components/BreathingTech.js
+++ src/components/BreathingTech.js
@@ -195,6 +195,19 @@ class BreathingTech extends React.Component {
     //     }
     // }
 
+    removeDuplicates = nums => {
+        if(nums.length === 0) return 0
+        
+        let p1 = 0
+        for(let p2 = 1; p2 < nums.length; p2++){
+            if(nums[p1] !== nums[p2]){
+                p1++
+                nums[p1] = nums[p2]
+            }
+        }
+        return p1 + 1
+    }
+
     componentDidMount(){
        this._isMounted = true 
        this.getOneBreathingTech()
