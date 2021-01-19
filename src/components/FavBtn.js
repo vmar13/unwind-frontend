adbe62a6910ab8cc2b9874e18343443b0462610e
github username <60378440+vmar13@users.noreply.github.com>
2021-01-19 13:41:57 -0500

    added blue heart and heart outline imgs for favBtn and adjusted positioning

--- src/components/FavBtn.js
+++ src/components/FavBtn.js
@@ -11,12 +11,12 @@ const FavBtn = ({favoriteBT, unFavoriteBT, BTname, favNames, allFavs}) => {
                 <button 
                 id={BTname}
                 className='favorite-btn'
-                onClick={() => unFavoriteBT(allFavs)}>Unfavorite</button> 
+                onClick={() => unFavoriteBT(allFavs)}><img src={require('../images/blue-heart.png')} alt='blue-heart' className='heart' /></button> 
                 : 
                 <button 
                 id={BTname}
                 className='favorite-btn'
-                onClick={favoriteBT}>Favorite</button>}
+                onClick={favoriteBT}><img src={require('../images/heart-outline.png')} alt='heart-outline' className='heart' /></button>}
 
                 
             </>
