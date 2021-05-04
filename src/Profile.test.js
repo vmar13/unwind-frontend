c6892d628d46d952c85b8acb662a4fcf977f441c
github username <60378440+vmar13@users.noreply.github.com>
2021-05-04 16:51:49 -0400

    removed profile test

--- src/Profile.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Profile from './components/Profile';
-
-
-test('should render without crashing', () => {
-  render(<Profile />);
-});
